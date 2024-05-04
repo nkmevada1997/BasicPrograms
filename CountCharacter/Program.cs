992e7b35014d8d48b69c4cf3aa7fe89516e0147a
Nayan K. Mevada <nkmevada1997@gmail.com>
2024-05-04 13:45:04 +0530

    - Palidrom String - Count Word From Sentence

---
+++ CountCharacter/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CountWords
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sentence = "Lorem Ipsum is simply dummy script of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the Lorem. Lorem Ipsum is not simply random text.";
+
+            Console.WriteLine("Enter the Word You Want to Find");
+            var input = Console.ReadLine();
+            var words = sentence.Split(' ');
+            var count = 0;
+            foreach (var word in words)
+            {
+                if (input.Equals(word, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"{input} : {count}");
+
+
+        }
+    }
+}
---
+++ PalindromeString/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PalindromeString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String");
+            var input = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                var result = ReverseString(input);
+
+                if (result.Equals(input, StringComparison.Ordinal))
+                {
+                    Console.WriteLine($"{input} is Palindrome");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is Not Palindrome");
+                }
+            }
+        }
+
+        private static string ReverseString(string value)
+        {
+            string result = string.Empty;
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                result += value[i];
+            }
+            return result;
+        }
+    }
+}
